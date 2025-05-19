3ecac822cab2c1ff64ba0a9be5f9bd5c4592e18a
DatHuynhCoder <htdat2004@gmail.com>
2025-05-19 09:00:12 +0700

    Add Toeic 945 Score

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,3 +1,5 @@
+import toeicscore from '../assets/Toeic_945.jpg';
+
 const Education = () => {
   return (
     <section id="education" className="border-b-2 pb-20 md:pb-40">
@@ -5,7 +7,8 @@ const Education = () => {
       <h3 className="text-xl md:text-2xl text-green-800 font-bold hover:underline hover:text-emerald-600">
         <a href="https://www.uit.edu.vn/" target="_blank" rel="noreferrer">University of Information Technology – UIT</a>
       </h3>
-      <p className="text-base md:text-lg font-medium">I am currently a third-year student majoring in Information Technology at UIT – Vietnam National University, Ho Chi Minh City. My current GPA is <b>8.66</b></p>
+      <p className="text-base md:text-lg font-medium">I am currently a third-year student majoring in Information Technology at UIT – Vietnam National University, Ho Chi Minh City. My current GPA is <b>8.66</b> and has TOEIC score of <b>945/990</b></p>
+      <img src={toeicscore} alt="TOEIC Score" className='my-4 object-cover w-3/5 mx-auto'/>
       <div className="my-3 font-bold text-green-600">Oct 2022 - Now</div>
       <ul className="text-justify text-base md:text-lg space-y-2">
         <li>📖 My academic journey has built a strong foundation in Information Technology. I chose to begin my path with a focus on web development, where I continuously develop both technical and creative skills.</li>
